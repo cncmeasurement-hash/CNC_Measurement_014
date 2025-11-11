a3341f8f05cd6f6f46e2b0b7fc1b4d7a1fc75878
cncmeasurement-hash <cnc.measurement@gmail.com>
2025-11-11 14:54:48 +0300

    update

--- obj/x64/Debug/net8.0-windows/014.AssemblyInfo.cs
+++ obj/x64/Debug/net8.0-windows/014.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("014")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37f39453aca8b76f75b8b8bc5cddaa090025272a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65b99ce4e724a66fe82989e313107dbbac407d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("014")]
 [assembly: System.Reflection.AssemblyTitleAttribute("014")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
